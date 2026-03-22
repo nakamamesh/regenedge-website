4ba6be80b835e8f4270d4be1f6f91a0c4b19409e
nakamamesh <maverickmexico2@gmail.com>
2026-03-22 13:21:27 -0500

    Add ReGenEdge Optimize page

---
+++ optimize/static/chunks/app/website-audit/layout-f2212f67a20d51dc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[90],{3485:function(n,e,u){Promise.resolve().then(u.t.bind(u,6988,23))},6988:function(){}},function(n){n.O(0,[53,971,117,744],function(){return n(n.s=3485)}),_N_E=n.O()}]);
